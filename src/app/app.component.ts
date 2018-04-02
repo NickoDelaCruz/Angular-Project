0f1effb0169fee26f58a5703d4a8fd8212c36e0d
Nicko <nickodc96@gmail.com>
2018-04-01 20:30:03 -0700

    add basic sie layout, getting undefined error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>DVD SHOP</h1>
+  <p> Here are some movies:</p>
+
+  <ul>
+      <li ngFor="#movieList" (click)="selectedMovie(movie)">{{movie}}</li>
+  </ul>
+
+
+  Recently Added Movie: {{recentMovie}}<br>
+
+  Cart:
+  <ul>
+      <li ngFor="#item">{{item}}</li>
+  </ul>
+
+
+  `
+
+
 })
 export class AppComponent {
-  title = 'app';
+  recentMovie = "None";
+  public cart = [];
+  public movieList = ['Harry Potter', 'Harry PotterOMG', 'Harry PotterRETURN']
+  selectedMovie(movie) {
+      alert(movie)
+  }
 }
