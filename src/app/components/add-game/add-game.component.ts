91d4ab83f8f2476d5f4b844c63b978989a50c5c8
Nicko <nickodc96@gmail.com>
2018-04-07 13:39:35 -0700

    add toggleDisplay and addListing in add-game.ts

--- src/app/components/add-game/add-game.component.ts
+++ src/app/components/add-game/add-game.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GameService } from '../../game.service';
+import { Agame } from '../../game.model';
+
+
 @Component({
   selector: 'app-add-game',
   templateUrl: './add-game.component.html',
   styleUrls: ['./add-game.component.css']
 })
+
 export class AddGameComponent implements OnInit {
 
     toggle: boolean = false;
 
-  constructor() { }
+  constructor(private gameService: GameService) { }
 
   ngOnInit() {
   }
+
+  toggleDisplay() {
+    this.toggle = !this.toggle;
+  }
+
+  addListing(owner: string, title: string, price: string, system: string, description: string) {
+    const newGame: Agame = new Agame(owner, title, price, system, description);
+
+    this.gameService.addGame(newGame);
+    this.toggleDisplay();
+  }
+
+
 
 }
