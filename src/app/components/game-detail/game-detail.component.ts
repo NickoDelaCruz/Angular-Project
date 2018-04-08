af393c8da1fac92cf5827f22a6586faf207d1b19
Nicko <nickodc96@gmail.com>
2018-04-07 18:20:25 -0700

    add code to game-detail.ts, used album-detail as ref

--- src/app/components/game-detail/game-detail.component.ts
+++ src/app/components/game-detail/game-detail.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { GameService } from '../../game.service';
 
 @Component({
   selector: 'app-game-detail',
   templateUrl: './game-detail.component.html',
-  styleUrls: ['./game-detail.component.css']
+  styleUrls: ['./game-detail.component.css'],
+  providers: [GameService]
 })
 export class GameDetailComponent implements OnInit {
 
-  constructor() { }
+  gameKey: string;
+  gameDetail;
+
+  constructor(private route: ActivatedRoute, private gameService: GameService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters)=> {
+      this.gameKey = urlParameters['id'];
+    });
+    this.gameDetail = this.gameService.getGameByKey(this.gameKey);
   }
 
 }
